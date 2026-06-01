ffdb39c1a0a2be70f96c5f66c3ff9e4fdf4b72d0
Aditya Ulya <adityaulyaa@gmail.com>
2026-06-01 23:40:54 +0700

    BBProg1

--- investment_optimizer/Algorithms/branch_bound.py
+++ investment_optimizer/Algorithms/branch_bound.py
@@ -32,6 +32,8 @@ def branch_and_bound(modal):
     explored_nodes = 0
     pruned_nodes = 0
 
+    all_valid_solutions = []
+
     # Branching
     for tabungan in range(
         10,
@@ -65,22 +67,41 @@ def branch_and_bound(modal):
                 modal
             )
 
+            all_valid_solutions.append(
+                solution
+            )
+
             if solution["wealth"] > best_wealth:
                 best_wealth = solution["wealth"]
                 best_solution = solution
 
     runtime = perf_counter() - start_time
 
+    # Urutkan berdasarkan wealth
+    all_valid_solutions.sort(
+        key=lambda x: x["wealth"],
+        reverse=True
+    )
+
+    top_3 = all_valid_solutions[:3]
+
     result = {
         "algorithm": "Branch and Bound",
+
         "tabungan": best_solution["tabungan"],
         "emas": best_solution["emas"],
         "reksa": best_solution["reksa"],
+
         "wealth": best_solution["wealth"],
         "risk": best_solution["risk"],
+
         "runtime": runtime,
-        "explored_nodes": explored_nodes,
-        "pruned_nodes": pruned_nodes
+
+        "total_generated": explored_nodes,
+        "pruned_count": pruned_nodes,
+        "valid_count": len(all_valid_solutions),
+
+        "top_3": top_3
     }
 
     return result
@@ -95,33 +116,46 @@ if __name__ == "__main__":
     print("\n=== BRANCH AND BOUND ===")
 
     print(
-        f"Tabungan : {result['tabungan']}%"
+        f"Total Kombinasi Dibangkitkan : {result['total_generated']}"
     )
 
     print(
-        f"Emas     : {result['emas']}%"
+        f"Kombinasi Terpruning         : {result['pruned_count']}"
     )
 
     print(
-        f"Reksa    : {result['reksa']}%"
+        f"Kombinasi Valid             : {result['valid_count']}"
     )
 
-    print(
-        f"Wealth   : Rp {result['wealth']:,.2f}"
-    )
+    print("\nTOP 3 SOLUSI")
+
+    for i, sol in enumerate(
+        result["top_3"],
+        start=1
+    ):
+
+        print(f"\n#{i}")
+
+        print(
+            f"Tabungan : {sol['tabungan']}%"
+        )
+
+        print(
+            f"Emas     : {sol['emas']}%"
+        )
+
+        print(
+            f"Reksa    : {sol['reksa']}%"
+        )
+
+        print(
+            f"Risk     : {sol['risk']:.2f}"
+        )
+
+        print(
+            f"Wealth   : Rp {sol['wealth']:,.2f}"
+        )
 
     print(
-        f"Risk     : {result['risk']:.2f}"
-    )
-
-    print(
-        f"Runtime  : {result['runtime']:.6f} detik"
-    )
-
-    print(
-        f"Explored : {result['explored_nodes']}"
-    )
-
-    print(
-        f"Pruned   : {result['pruned_nodes']}"
+        f"\nRuntime  : {result['runtime']:.6f} detik"
     )
